bcc8a84f3e9bcf95190fbfa7b1641d0e46e7852d
VISKI NATALINA <overlordnatalina@gmail.com>
2019-09-07 16:37:09 +0300

    Clone for gitlab

--- task07/script07.js
+++ task07/script07.js
@@ -1,11 +1,8 @@
 //- Спросите у пользователя текст, и проверьте его на наличие некорректного слова или нескольких некорректных слов.
 // Используйте метод indexOf (или includes) строки:
 
-let str=prompt("Input some sentence")
-
- 
-    if(str.indexOf()){
-    alert("You input incorrect sentence, I couldn't interpritat it")
-   
+let str=prompt("Input some sentence") 
+    if(str.indexOf('1')){
+    alert("You input incorrect sentence, I couldn't interpritat it")   
    }
 
